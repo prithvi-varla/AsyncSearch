9788f09ecbb46a3c8b1864848b304b009e503f0f
prithvirajv <prithviraj.varla@concur.com>
2018-08-27 20:34:07 -0500

    Initial checkin

---
+++ src/main/java/com/example/AsyncSearch/AsyncSearchApplication.java
@@ -0,0 +1,12 @@
+package com.example.AsyncSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncSearchApplication.class, args);
+	}
+}
